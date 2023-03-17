d7416bcf2e50e715b79ffef1f007c132ac280dfb
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-03-17 11:45:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+ function saturdayFun(todo="roller-skate") {
+    return `This Saturday, I want to ${todo}!`
+ }
+ function mondayWork(todo="go to the office") {
+    return `This Monday, I will ${todo}.`
+ }
+ function wrapAdjective(string="*") {
+    return function(parameter="a hard worker") {
+        return `You are ${string}${parameter}${string}!`
+
+    }
+ }
+
+
+
+
+
+
